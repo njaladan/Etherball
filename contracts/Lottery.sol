74787924dd767bf1bf7bcc1b53fbc539cb585e39
Ganesh <ganesh.jaladanki@gmail.com>
2018-01-05 17:30:17 -0800

    I need to get the files on my laptop lol

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -9,16 +9,20 @@ pragma solidity ^0.4.17;
 
 contract Lottery {
 
-    event LotteryTicketPurchased(address index _purchaser, uint256 _ticketID)
-    event LotteryAmountPaid(address index _winner, uint256 _amount);
+    event LotteryTicketPurchased(address indexed _purchaser, uint256 _ticketID);
+    event LotteryAmountPaid(address indexed _winner, uint256 _amount);
     event OwnershipTransferred(address _old, address _new);
 
-    mapping(uint16 => address) public ticketMapping;
-    uint256 ticketsBought = 0;
 
     // variables that I may want to change in the future
-    var ticketPrice = 10 finney;
-    var ticketMax = 100;
+    uint64 ticketPrice = 10 finney;
+    uint64 ticketMax = 10;
+
+    // number of tickets is set to a hard 100, I hope I don't regret this
+    address[10] public ticketMapping;
+    uint256 ticketsBought = 0;
+
+    address owner;
 
     modifier onlyOwner()  {
       require(msg.sender==owner);
@@ -31,87 +35,67 @@ contract Lottery {
       _;
     }
 
-    function Lottery(){
+    function Lottery() public{
       owner = msg.sender;
-    }
-
-    function changeState(uint256 _ticketPrice, uint16 _ticketMax) onlyOwner returns (bool) {
-      // just because I know I'll mess up
-      // doesn't worry about case where sold > new max because owner can change it anyway
-      // assumes a benevolent owner, not sure if this can even be malicious though
-
-      require(_ticketPrice > 0);
-      require(_ticketMax > 0);
-
-      ticketPrice = _ticketPrice;
-      ticketMax = _ticketMax;
-      return true;
     }
 
     function changeOwner(address _replacement) onlyOwner public returns (bool) {
       // adding this because I don't trust myself yet
       require(_replacement != address(0));
-      old = owner;
+      address old = owner;
       owner = _replacement;
       OwnershipTransferred(old, owner);
       return true;
     }
 
 
-    function() payable {
+    function() payable public {
       // for now, have ticket purchasing only through functions
       // for sanity purposes
       revert();
     }
 
-    function buyTicket(uint16 _ticket) payable returns (bool) {
+    function buyTicket(uint16 _ticket) payable public returns (bool) {
       // I'd prefer all tickets to just be 0.01 ether
-      // TODO: buy multiple tickets in one round
-      require(msg.value == ticketPrice);
-      require(_ticket > 0 && _ticket < ticketMax);
-      require(ticketMapping[_ticket]==0);
+      // require(msg.value == ticketPrice);
+      require(_ticket > 0 && _ticket < ticketMax+1);
+      require(ticketMapping[_ticket-1]==address(0));
       require(ticketsBought < ticketMax);
 
-      purchaser = msg.sender;
+      address purchaser = msg.sender;
       ticketsBought += 1;
-      ticketMapping[_ticket] = purchaser;
-
-      // not sure if it's okay to unload the gas cost on the poor fellow
-      // that bought the last one.
-      // TODO: check gas usage with and without function
-      if (ticketsBought >= ticketMax) {
-        sendReward();
-      }
+      ticketMapping[9] = purchaser;
+      ticketMapping[_ticket-1] = purchaser;
+      LotteryTicketPurchased(purchaser, _ticket);
 
       return true;
     }
 
-    // is this function necessary if ticketsBought == ticketMax?
-    function ticketCheck() internal returns (bool);
-
     // if a bad winner is chosen the first time, it's possible to just run
     // sendReward() again. But can this cause an attack?
-    function sendReward() allTicketsSold returns (address) {
-      address winner = lotteryPicker();
+    function sendReward() public allTicketsSold returns (address) {
+      //address winner = lotteryPicker();
       // prevent locked funds by sending to bad address
-      require(winner != address(0))
-      totalAmount = ticketMax*ticketPrice;
+      address winner = address(1);
+      require(winner != address(0));
+      uint256 totalAmount = ticketMax*ticketPrice;
       reset();
       winner.transfer(totalAmount);
+      LotteryAmountPaid(winner, totalAmount);
+      return winner;
     }
 
-    function lotteryPicker() private allTicketsSold returns (address) {
-      var randomness = uint64(ripemd160(block.coinbase, block.number, block.timestamp));
-      uint16 winnerTicket = randomness % ticketMax;
-      return ticketMapping[winnerTicket];
+    function lotteryPicker() public view returns (bytes32) {
+      uint256 hello = 12*ticketMax;
+
+      return block.blockhash(block.number);
     }
 
     function reset() private allTicketsSold returns (bool) {
       ticketsBought = 0;
-      delete ticketMapping;
-
+      for(uint x = 0; x < ticketMax; x++) {
+        delete ticketMapping[x];
+      }
+      return true;
     }
-
-    function getState() public;
-
 }

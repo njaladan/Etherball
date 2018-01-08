99bab1611fa809c7aa933db6043db704a7fae1ce
unknown <ganesh.jaladanki@gmail.com>
2018-01-08 00:08:12 -0800

    added live ether price tracking

--- src/js/ethprice.js
+++ src/js/ethprice.js
@@ -3,7 +3,7 @@ $.getJSON(
   "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD",
   function(json) {
     console.log(json['USD']);
-    $('#poolPrice').text(0.125*json['USD']);
-    $('#ticketPrice').text(0.005*json['USD']);
+    $('#poolPrice').text((0.125*json['USD']).toFixed(2));
+    $('#ticketPrice').text((0.005*json['USD']).toFixed(2));
   }
 );
